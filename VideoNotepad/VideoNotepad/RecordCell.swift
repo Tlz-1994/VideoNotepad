7a6edf922052c82c229fe9d4ed9ffd4904ed579e
stefanie <tanlizhi130@gmail.com>
2017-09-17 11:58:57 +0800

    设置音频记录cell的背景色

--- VideoNotepad/VideoNotepad/RecordCell.swift
+++ VideoNotepad/VideoNotepad/RecordCell.swift
@@ -28,6 +28,7 @@ class RecordCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        contentView.backgroundColor = UIColor.white
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
